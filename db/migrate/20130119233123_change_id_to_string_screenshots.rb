a7e73590127de6cec44c2eca439285c81f16bbbd
Dustin Smith <dustinsmith1024@hotmail.com>
2013-01-26 16:38:43 -0600

    removing bad stuff

--- db/migrate/20130119233123_change_id_to_string_screenshots.rb
+++
@@ -1,9 +0,0 @@
-class ChangeIdToStringScreenshots < ActiveRecord::Migration
-  def up
-  	change_column :screenshots, :id, :string
-  end
-
-  def down
-  	change_column :screenshots, :id, :integer
-  end
-end
